b0ad8ae3ce3fa4d73f24466e9ca62e26297db051
menikdp <menik.damayanti.p@gmail.com>
2018-11-23 21:34:16 +0700

    add main menu feature

---
+++ features/Phptravels.feature
@@ -0,0 +1,9 @@
+@Phptravels
+Feature: Php Travels
+
+    @MainMenu
+    Scenario: MainMenu
+        Given open the website
+        Then user click on main menu
+        Then user closing the browser
+
---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,25 @@
+require 'selenium-webdriver'
+require 'rubygems'
+require 'rspec'
+
+driver = Selenium::WebDriver.for:chrome
+
+Given("open the website") do
+    driver.navigate.to "https://phptravels.com/demo/"
+end
+
+Then("user click on main menu") do
+    driver.find_element(:link, 'DEMO').click
+    driver.find_element(:link, 'PRICING').click
+    driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[3]/span/span').click
+    driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[4]/span/span').click
+    driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[5]/span/span').click
+    driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[6]/span/span').click
+    driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[7]/span/span').click
+    driver.find_element(:link,'LOGIN').click
+    sleep(2)
+end
+
+Then("user closing the browser") do
+    driver.quit
+end
