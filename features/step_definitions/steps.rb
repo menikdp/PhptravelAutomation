1d885d8aa1d4a09e07eb0d2943a77f8858ece89f
menikdp <menik.damayanti.p@gmail.com>
2018-11-27 15:39:42 +0700

    add popup handler

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,25 +1,27 @@
 require 'selenium-webdriver'
 require 'rubygems'
 require 'rspec'
+# require_relative 'main_menu'
+
+caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => [ "--disable-web-security" ]})
 
 driver = Selenium::WebDriver.for:chrome
-
 Given("open the website") do
     driver.navigate.to "https://phptravels.com/demo/"
     driver.manage.window.maximize
 end
 
 Then("user click on main menu") do
-    driver.find_element(:link, 'DEMO').click
-    driver.find_element(:link, 'PRICING').click
-    driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[3]/span/span').click
-    driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[4]/span/span').click
-    driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[5]/span/span').click
-    driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[6]/span/span').click
-    driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[7]/span/span').click
-    driver.find_element(:link,'LOGIN').click
-    sleep(2)
-end
+  driver.find_element(:link, 'DEMO').click
+  driver.find_element(:link, 'PRICING').click
+  driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[3]/span/span').click
+  driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[4]/span/span').click
+  driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[5]/span/span').click
+  driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[6]/span/span').click
+  driver.find_element(:xpath,'//*[@id="main-menu"]/ul/li[7]/span/span').click
+  driver.find_element(:link,'LOGIN').click
+  sleep(2)
+end  
 
 Then("user closing the browser") do
     driver.quit
