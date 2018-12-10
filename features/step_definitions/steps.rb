ad2bd4abdfa960c5169c064cbbc40770c8e7f46e
menikdp <menik.damayanti.p@gmail.com>
2018-12-10 17:35:58 +0700

    rewrite handler popup, comment for SignIn scenario

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -2,17 +2,24 @@ require 'selenium-webdriver'
 require 'rubygems'
 require 'rspec'
 
-# caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => [ "--disable-web-security" ]})
-options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument('--disable-notifications')
-options.add_argument('--disable-popup-blocking')
-driver = Selenium::WebDriver.for :chrome, options: options
+driver = Selenium::WebDriver.for :chrome
 
 Given("open the website") do
     driver.navigate.to "https://phptravels.com/demo/"
     driver.manage.window.maximize
-    # driver.find_element(:xpath, '//*[@id="PopupSignupForm_0"]/div[2]').displayed?
-    # driver.find_element(:xpath, '//*[@id="PopupSignupForm_0"]/div[2]/div[1]').click
+    
+    #popup handling
+    if driver.find_elements(:id,'onesignal-popover-cancel-button').size > 0
+      driver.find_element(:id, 'onesignal-popover-cancel-button').click
+    end
+
+    if driver.find_elements(:class, 'modalContent').size > 0
+      driver.find_element(:class, 'mc-closeModal').click
+    end
+end
+
+Then("user closing the browser") do
+  driver.quit
 end
 
 Then("user click on main menu") do
@@ -27,14 +34,39 @@ Then("user click on main menu") do
   sleep(2)
 end  
 
-Then("user closing the browser") do
-    driver.quit
+When("user choose homepage frontend") do
+  driver.find_element(:xpath, '/html/body/section[2]/div/div/div[1]/div/div/div[2]/div[2]/div/div[1]/div/a').click
 end
 
-When("user choose home-page frontend") do
+Then("user should on homepage frontend") do
+  puts "Title " if wait.until{ /www.phptravels.net/.match(driver.current_url)}
+end
+
+When("user click on My Account") do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then("user should on home-page frontend") do
+When("user choose login") do
   pending # Write code here that turns the phrase above into concrete actions
 end
+
+Then("user should on login page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("user fill email") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("user fill password") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("user click on login button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("user should logged in") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
